1ac5070c536ba6a1f76c174e5578275cfc2a86d3
Richard Lawrence <richard@humanizingwork.com>
2011-02-14 15:54:02 -0700

    Initial commit after setting up rails.

---
+++ Gemfile
@@ -0,0 +1,10 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.4'
+gem 'sqlite3'
+
+group :development, :test do
+  gem "rspec-rails", ">= 2.5.0"
+  gem "cucumber", "~> 0.10.0"
+  gem "cucumber-rails"
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PassengerPickups::Application.config.secret_token = '31e79876d0ff0b6d42d545adcaed4c6022d741d17f867b332c85c268be478b184d3687a2fde0c7eb7a99911a85defadc0df1273cfc7d21251807529474eaa661'
