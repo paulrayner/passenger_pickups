c6d8a09cbf1700a5f4454c56c0ffbb1ddbdc3687
Richard Lawrence <richard@humanizingwork.com>
2011-03-08 21:06:54 -0700

    Set today's arrivals to work around service due to process limitation.

--- app/models/arrival.rb
+++ app/models/arrival.rb
@@ -1,10 +1,17 @@
 class Arrival < ActiveRecord::Base
   def flight_status
-    #@flight_status ||= get_flight_status rescue nil
-    get_flight_status
+    @flight_status ||= check_flight_status rescue nil
   end
 
-  def get_flight_status
-    Flight.status(Date.parse(self.arrival_time.to_s), self.flight_number, self.from)
+  def check_flight_status
+    Flight.where(
+            :flight_number => self.flight_number,
+            :scheduled_time => DateTime.parse("#{self.arrival_date} 00:00")..DateTime.parse("#{self.arrival_date} 23:59"),
+            :from => self.from
+    ).first
+  end
+
+  def arrival_date
+    self.arrival_time.strftime('%m/%d/%Y')
   end
 end
