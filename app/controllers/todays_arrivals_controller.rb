00dfbe25d8aa7815929b1c04a1a21fd86f89a328
Richard Lawrence <richard@humanizingwork.com>
2011-03-08 20:54:14 -0700

    Updated service code.

--- app/controllers/todays_arrivals_controller.rb
+++ app/controllers/todays_arrivals_controller.rb
@@ -1,5 +1,5 @@
 class TodaysArrivalsController < ApplicationController
   def index
-    @arrivals = Arrival.where(:arrival_time => DateTime.parse("00:00")..DateTime.parse("23:59"))
+    @arrivals = Arrival.where(:arrival_time => DateTime.parse("00:00")..DateTime.parse("23:59")).order(:arrival_time)
   end
 end
--- app/models/arrival.rb
+++ app/models/arrival.rb
@@ -1,13 +1,10 @@
 class Arrival < ActiveRecord::Base
   def flight_status
-    @flight_status || check_flight_status
+    #@flight_status ||= get_flight_status rescue nil
+    get_flight_status
   end
 
-  def check_flight_status
-    @flight_status = FlightStatus.status(Date.parse(self.arrival_time.to_s), self.flight_number, self.from)
-  end
-
-  def flight_status_available?
-    !flight_status.nil?
+  def get_flight_status
+    Flight.status(Date.parse(self.arrival_time.to_s), self.flight_number, self.from)
   end
 end
